16f5170a31f8c1cbf1449e7bb11cb49fc3f4290a
Brayan Arriola <brayanarriola0@gmail.com>
2022-06-12 16:13:28 -0600

    subir sitio web

---
+++ platillos/java/java.js
@@ -0,0 +1,132 @@
+var menu__phone = document.getElementById("menu__phone");
+var imgA__header = document.getElementById("imgA__header");
+var imgB__header = document.getElementById("imgB__header");
+
+var transparent__fondoA = document.getElementById("transparent__fondoA");
+var transparent__fondoB = document.getElementById("transparent__fondoB");
+var transparent__fondoC = document.getElementById("transparent__fondoC");
+var transparent__fondoD = document.getElementById("transparent__fondoD");
+var transparent__fondoE = document.getElementById("transparent__fondoE");
+var transparent__fondoF = document.getElementById("transparent__fondoF");
+var transparent__fondoG = document.getElementById("transparent__fondoG");
+var transparent__fondoH = document.getElementById("transparent__fondoH");
+var transparent__fondoI = document.getElementById("transparent__fondoI");
+
+
+/*funciones para mostrar el menu en phone*/ 
+function mostrarMenu(){
+    menu__phone.style.display = 'flex';
+    imgA__header.style.display = 'none';
+    imgB__header.style.display = 'flex';
+}
+function ocultarMenu(){
+    menu__phone.style.display = 'none';
+    imgB__header.style.display = 'none';
+    imgA__header.style.display = 'flex';
+}
+
+
+/*funciones para mostrar el block que da la opcion de redirigir al menu*/
+
+function mostrarplatilloA(){
+    transparent__fondoA.style.display = 'block';
+    transparent__fondoB.style.display = 'none';
+    transparent__fondoC.style.display = 'none';
+    transparent__fondoD.style.display = 'none';
+    transparent__fondoE.style.display = 'none';
+    transparent__fondoF.style.display = 'none';
+    transparent__fondoG.style.display = 'none';
+    transparent__fondoH.style.display = 'none';
+    transparent__fondoI.style.display = 'none';
+}
+
+function mostrarplatilloB(){
+    transparent__fondoB.style.display = 'block';
+    transparent__fondoA.style.display = "none";
+    transparent__fondoC.style.display = 'none';
+    transparent__fondoD.style.display = 'none';
+    transparent__fondoE.style.display = 'none';
+    transparent__fondoF.style.display = 'none';
+    transparent__fondoG.style.display = 'none';
+    transparent__fondoH.style.display = 'none';
+    transparent__fondoI.style.display = 'none';
+}
+
+function mostrarplatilloC(){
+    transparent__fondoC.style.display = 'block';
+    transparent__fondoA.style.display = "none";
+    transparent__fondoB.style.display = 'none';
+    transparent__fondoD.style.display = 'none';
+    transparent__fondoE.style.display = 'none';
+    transparent__fondoF.style.display = 'none';
+    transparent__fondoG.style.display = 'none';
+    transparent__fondoH.style.display = 'none';
+    transparent__fondoI.style.display = 'none';
+}
+
+function mostrarplatilloD(){
+    transparent__fondoD.style.display = 'block';
+    transparent__fondoA.style.display = "none";
+    transparent__fondoB.style.display = 'none';
+    transparent__fondoC.style.display = 'none';
+    transparent__fondoE.style.display = 'none';
+    transparent__fondoF.style.display = 'none';
+    transparent__fondoG.style.display = 'none';
+    transparent__fondoH.style.display = 'none';
+    transparent__fondoI.style.display = 'none';
+}
+function mostrarplatilloE(){
+    transparent__fondoE.style.display = 'block';
+    transparent__fondoA.style.display = "none";
+    transparent__fondoB.style.display = 'none';
+    transparent__fondoC.style.display = 'none';
+    transparent__fondoD.style.display = 'none';
+    transparent__fondoF.style.display = 'none';
+    transparent__fondoG.style.display = 'none';
+    transparent__fondoH.style.display = 'none';
+    transparent__fondoI.style.display = 'none';
+}
+function mostrarplatilloF(){
+    transparent__fondoF.style.display = 'block';
+    transparent__fondoA.style.display = "none";
+    transparent__fondoB.style.display = 'none';
+    transparent__fondoC.style.display = 'none';
+    transparent__fondoD.style.display = 'none';
+    transparent__fondoE.style.display = 'none';
+    transparent__fondoG.style.display = 'none';
+    transparent__fondoH.style.display = 'none';
+    transparent__fondoI.style.display = 'none';
+}
+function mostrarplatilloG(){
+    transparent__fondoG.style.display = 'block';
+    transparent__fondoA.style.display = "none";
+    transparent__fondoB.style.display = 'none';
+    transparent__fondoC.style.display = 'none';
+    transparent__fondoD.style.display = 'none';
+    transparent__fondoE.style.display = 'none';
+    transparent__fondoF.style.display = 'none';
+    transparent__fondoH.style.display = 'none';
+    transparent__fondoI.style.display = 'none';
+}
+function mostrarplatilloH(){
+    transparent__fondoH.style.display = 'block';
+    transparent__fondoA.style.display = "none";
+    transparent__fondoB.style.display = 'none';
+    transparent__fondoC.style.display = 'none';
+    transparent__fondoD.style.display = 'none';
+    transparent__fondoE.style.display = 'none';
+    transparent__fondoF.style.display = 'none';
+    transparent__fondoG.style.display = 'none';
+    transparent__fondoI.style.display = 'none';
+}
+function mostrarplatilloI(){
+    transparent__fondoI.style.display = 'block';
+    transparent__fondoA.style.display = "none";
+    transparent__fondoB.style.display = 'none';
+    transparent__fondoC.style.display = 'none';
+    transparent__fondoD.style.display = 'none';
+    transparent__fondoE.style.display = 'none';
+    transparent__fondoF.style.display = 'none';
+    transparent__fondoG.style.display = 'none';
+    transparent__fondoH.style.display = 'none';
+}
