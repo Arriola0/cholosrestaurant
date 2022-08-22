c11c48806ab9b5480d5a5591d9a124f85006ff9b
Brayan Arriola <brayanarriola0@gmail.com>
2022-08-21 23:39:34 -0600

    Actualizacion1 - agrege funcion en section ofertas

--- js/java.js
+++ js/java.js
@@ -3,6 +3,19 @@ var imgA__header = document.getElementById("imgA__header");
 var imgB__header = document.getElementById("imgB__header");
 
 
+let transparent1 = document.getElementById("transparent1");
+let transparent2 = document.getElementById("transparent2");
+let transparent3 = document.getElementById("transparent3");
+let transparent4 = document.getElementById("transparent4");
+let transparent5 = document.getElementById("transparent5");
+let transparent6 = document.getElementById("transparent6");
+let transparent7 = document.getElementById("transparent7");
+let transparent8 = document.getElementById("transparent8");
+let transparent9 = document.getElementById("transparent9");
+let transparent10 = document.getElementById("transparent10");
+let transparent11 = document.getElementById("transparent11");
+
+
 function mostrarMenu(){
     menu__phone.style.display = 'flex';
     imgA__header.style.display = 'none';
@@ -15,8 +28,82 @@ function ocultarMenu(){
 }
 
 
+/*Funcion para que aparesca el boton en los platillos*/
+
+function cerrar1(){
+    transparent1.style.display = 'none';
+}
+function cerrar2(){
+    transparent2.style.display = 'none';
+}
+function cerrar3(){
+    transparent3.style.display = 'none';
+}
+function cerrar4(){
+    transparent4.style.display = 'none';
+}
+function cerrar5(){
+    transparent5.style.display = 'none';
+}
+function cerrar6(){
+    transparent6.style.display = 'none';
+}
+function cerrar7(){
+    transparent7.style.display = 'none';
+}
+function cerrar8(){
+    transparent8.style.display = 'none';
+}
+function cerrar9(){
+    transparent9.style.display = 'none';
+}
+function cerrar10(){
+    transparent10.style.display = 'none';
+}
+function cerrar11(){
+    transparent11.style.display = 'none';
+}
 
 
+
+
+
+
+
+
+function mostrarPlatillo1(){
+    transparent1.style.display ='block';
+}
+function mostrarPlatillo2(){
+    transparent2.style.display ='block';
+}
+function mostrarPlatillo3(){
+    transparent3.style.display ='block';
+}
+function mostrarPlatillo4(){
+    transparent4.style.display ='block';
+}
+function mostrarPlatillo5(){
+    transparent5.style.display ='block';
+}
+function mostrarPlatillo6(){
+    transparent6.style.display ='block';
+}
+function mostrarPlatillo7(){
+    transparent7.style.display ='block';
+}
+function mostrarPlatillo8(){
+    transparent8.style.display ='block';
+}
+function mostrarPlatillo9(){
+    transparent9.style.display ='block';
+}
+function mostrarPlatillo10(){
+    transparent10.style.display ='block';
+}
+function mostrarPlatillo11(){
+    transparent11.style.display ='block';
+}
 
 
 
